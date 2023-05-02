0f8c22c9616d51379d51c4cf8793122c53938152
Mika-Matti Auerkallio <mikamatti.auerkallio@gmail.com>
2023-05-02 03:02:58 +0300

    Write a test for createDataBaseConnection to server.test.ts

--- server/__tests__/server.test.ts
+++ server/__tests__/server.test.ts
@@ -1,24 +1,46 @@
 import { Express } from "express";
-import { Connection } from "mysql";
+import { createConnection, Connection } from "mysql";
 import { createServer, createDatabaseConnection } from "../src/server";
 const request = require("supertest");
 
-describe("Test creating a server", () => {
+describe("Test createServer", () => {
   let server: Express;
-  let connection: Connection;
 
   beforeAll(() => {
     server = createServer();
-    connection = createDatabaseConnection();
-  });
-
-  afterAll(() => {
-    connection.end();
   });
 
   it('should return "Hello" when GET /', async () => {
     const response = await request(server).get("/");
     expect(response.status).toBe(200);
     expect(response.text).toBe("Hello");
+  });
+});
+
+describe("Test createDataBaseConnection", () => {
+  let connection: Connection;
+
+  beforeAll(() => {
+    connection = createDatabaseConnection();
+  });
+
+  afterAll((done) => {
+    connection.end((err) => {
+      if (err) {
+        done.fail(err);
+      }
+      done();
+    });
+  });
+
+  it("should connect to the database", (done) => {
+    connection.on("error", (err) => {
+      done.fail(err);
+    });
+
+    connection.on("connect", () => {
+      expect(connection.state).toBe("connected");
+      done();
+    });
   });
 });
