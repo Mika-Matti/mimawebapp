0298cb7dd9cdb601259ec0aec45142398ea6f99f
Mika-Matti Auerkallio <mikamatti.auerkallio@gmail.com>
2023-05-10 19:16:44 +0300

    Create images folder to assets folder in client. Add a logo and replace default logo with it in HomeView.vue. Import a font and update the .scss files to use it.

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <img alt="mimanet logo" src="../assets/images/mimanetlogo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
