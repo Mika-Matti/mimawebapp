008b060c1e6248bd9b0e88f183df888eb1a8038d
Mika-Matti Auerkallio <mikamatti.auerkallio@gmail.com>
2023-05-11 16:32:30 +0300

    Rename default helloworld component into homepage

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="home">
     <img alt="mimanet logo" src="../assets/images/mimanetlogo128.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <HomePage msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import HomePage from "@/components/HomePage.vue"; // @ is an alias to /src
 
 @Options({
   components: {
-    HelloWorld,
+    HomePage,
   },
 })
 export default class HomeView extends Vue {}
