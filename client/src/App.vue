3cf4d85cc4e2470ee7201be47b9828b8130ef058
Mika-Matti Auerkallio <mikamatti.auerkallio@gmail.com>
2023-05-11 16:18:16 +0300

    Add empty portfolio viewand link to this page to the navbar in client.

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link class="router-link" to="/">Home</router-link>
+    <router-link class="router-link" to="/projects">Portfolio</router-link>
+    <router-link class="router-link" to="/about">About</router-link>
   </nav>
   <router-view />
 </template>
---
+++ client/src/views/ProjectsView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="projects">
+    <h1>This is the projects page</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({})
+export default class ProjectsView extends Vue {}
+</script>
