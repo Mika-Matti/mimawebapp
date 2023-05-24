383e51b56e4986ce41475d2a6b66e8b59c08b08a
Mika-Matti Auerkallio <mikamatti.auerkallio@gmail.com>
2023-05-24 10:54:22 +0300

    Add getProjectById for fetching project specific data either from existing projects state or from server in the vue store. Use this method for fetching id-specific project in projectView.vue.

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,26 +1,50 @@
 import { createStore } from "vuex";
 import axios from "axios";
 import { API_URLS } from "@/config";
+import { Project } from "@/types";
 
 export default createStore({
   state: {
-    projects: [],
+    projects: [] as Project[],
+    project: null as Project | null,
   },
   getters: {
     getProjects: (state) => state.projects,
+    getProject: (state) => state.project,
   },
   mutations: {
-    setProjects: (state, projects) => {
+    setProjects: (state, projects: Project[]) => {
       state.projects = projects;
+    },
+    setProject: (state, project) => {
+      state.project = project;
     },
   },
   actions: {
+    // Fetch projects
     async fetchProjects({ commit }) {
       try {
         const response = await axios.get(API_URLS.projects);
         commit("setProjects", response.data);
       } catch (error) {
         console.error("Error fetching projects:", error);
+      }
+    },
+    // Fetch project by id
+    async fetchProjectById({ commit, state }, id) {
+      const existingProject = state.projects.find(
+        (project) => project.project_id === id
+      );
+
+      if (existingProject) {
+        commit("setProject", existingProject);
+      } else {
+        try {
+          const response = await axios.get(API_URLS.project(id));
+          commit("setProject", response.data);
+        } catch (error) {
+          console.error("Error fetching project by id:", error);
+        }
       }
     },
   },
--- client/src/views/ProjectView.vue
+++ client/src/views/ProjectView.vue
@@ -7,6 +7,7 @@
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
+import { useStore } from "vuex";
 import PageHeader from "@/components/PageHeader.vue";
 import ProjectPage from "@/components/ProjectPage.vue"; // @ is an alias to /src
 
@@ -19,9 +20,21 @@ import ProjectPage from "@/components/ProjectPage.vue"; // @ is an alias to /src
 export default class ProjectView extends Vue {
   project_id = "-1";
   project = {};
+  store = useStore();
+
+  // Fetch project by id from server
+  async fetchProjectById(id: string) {
+    try {
+      await this.store.dispatch(`fetchProjectById`, id);
+      this.project = this.store.getters.getProject;
+    } catch (error) {
+      console.error("Failed to fetch project by id", error);
+    }
+  }
 
   mounted() {
     this.project_id = this.$route.params.id[0];
+    this.fetchProjectById(this.project_id);
   }
 }
 </script>
