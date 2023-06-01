8581b27cb5d759ee75c03868c52e741bf8436af2
Mika-Matti Auerkallio <mikamatti.auerkallio@gmail.com>
2023-06-01 19:35:14 +0300

    Write tests on server for authenticateUser in auth.controller.test.ts

--- server/__tests__/controllers/auth.controller.test.ts
+++ server/__tests__/controllers/auth.controller.test.ts
@@ -1,15 +1,187 @@
 import { Request, Response } from "express";
+import { hashSync } from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { User } from "../../src/models/user.model";
+import { db } from "../../src/utils/db";
 import {
   authenticateUser,
   logoutUser,
 } from "../../src/controllers/auth.controller";
 
-//TODO test authenticateUser POST /auth/login
-//TODO it should return code 400 with message "Invalid username or password" if username is invalid
-//TODO it should return code 400 with message "Invalid username or password" if password is invalid
-//TODO It should return code 401 with message "Invalid username or password" if user is not found
-//TODO It should return code 401 with message "Invalid username or password" if password doesn't match
-//TODO It should return code 200 with message "User authentication successful" if user is valid
+const mockQuery = db.query as jest.Mock;
+jest.mock("../../src/utils/db", () => ({
+  db: {
+    query: jest.fn(),
+  },
+}));
+
+jest.mock("jsonwebtoken", () => ({
+  sign: jest.fn().mockReturnValue("validToken"),
+}));
+
+// test authenticateUser
+describe("POST /auth/login", () => {
+  beforeAll(() => {
+    mockQuery.mockImplementation((sql: string, values?: any[]) => {
+      return new Promise<any>((resolve, reject) => {
+        console.log("Calling mocked POST /auth/login");
+        const expectedQuery: string = "SELECT * FROM users WHERE user_name = ?";
+        const passwordHash: string = hashSync("validPassword", 10);
+        const expectedUser: User = {
+          user_id: 1,
+          user_name: "validUsername",
+          user_password: passwordHash,
+          user_email: "valid@email.com",
+          user_role: "validRole",
+        };
+        const testUsername: string =
+          values && values.length > 0 ? values[0] : "";
+
+        if (sql !== expectedQuery) {
+          reject("Error: Mock function got wrong query");
+        }
+
+        console.log(
+          "expected: '" +
+            expectedUser.user_name +
+            "' got: '" +
+            testUsername +
+            "'"
+        );
+
+        if (expectedUser.user_name === testUsername) {
+          resolve([
+            {
+              user_id: expectedUser.user_id,
+              user_name: expectedUser.user_name,
+              user_password: expectedUser.user_password,
+              user_email: expectedUser.user_email,
+              user_role: expectedUser.user_role,
+            },
+          ]);
+        } else {
+          resolve([]);
+        }
+      });
+    });
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should return code 400 with expected message if username is invalid", async () => {
+    const req: Request = {
+      body: {
+        username: "invalidUserName<script>evilcode</script>",
+        password: "validPassword",
+      },
+    } as unknown as Request;
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    } as unknown as Response;
+
+    const expectedMessage: string = "Invalid username or password";
+
+    await authenticateUser(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.json).toHaveBeenCalledWith({
+      message: expectedMessage,
+    });
+  });
+
+  it("should return code 400 with expected message if password is invalid", async () => {
+    const req: Request = {
+      body: {
+        username: "validUsername",
+        password: "invalidPassword<script>evilcode</script>",
+      },
+    } as unknown as Request;
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    } as unknown as Response;
+
+    const expectedMessage: string = "Invalid username or password";
+
+    await authenticateUser(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.json).toHaveBeenCalledWith({
+      message: expectedMessage,
+    });
+  });
+
+  it("should return code 401 with expected message if valid username is not found", async () => {
+    const req: Request = {
+      body: {
+        username: "validButWrongUsername",
+        password: "validPassword",
+      },
+    } as unknown as Request;
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    } as unknown as Response;
+
+    const expectedMessage: string = "Invalid username or password";
+
+    await authenticateUser(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(401);
+    expect(res.json).toHaveBeenCalledWith({
+      message: expectedMessage,
+    });
+  });
+
+  it("should return code 401 with expected message if valid user, but password doesn't match", async () => {
+    const req: Request = {
+      body: {
+        username: "validUsername",
+        password: "invalidPassword",
+      },
+    } as unknown as Request;
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    } as unknown as Response;
+
+    const expectedMessage: string = "Invalid username or password";
+
+    await authenticateUser(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(401);
+    expect(res.json).toHaveBeenCalledWith({
+      message: expectedMessage,
+    });
+  });
+
+  it("should return 200 with expected message and token if valid user and password match", async () => {
+    const req: Request = {
+      body: {
+        username: "validUsername",
+        password: "validPassword",
+      },
+    } as unknown as Request;
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    } as unknown as Response;
+
+    const expectedMessage: string = "User authentication successful";
+    const expectedToken: string = "validToken";
+
+    await authenticateUser(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith({
+      message: expectedMessage,
+      token: expectedToken,
+    });
+  });
+});
 
 // test logoutUser
 describe("POST /auth/logout", () => {
--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
+import * as bcrypt from "bcrypt";
+import * as jwt from "jsonwebtoken";
 import { User } from "../models/user.model";
 import { config } from "../config";
 import { db } from "../utils/db";
