741c5a028379941c00ff3d5dadf4060a0dd48921
Mika-Matti Auerkallio <mikamatti.auerkallio@gmail.com>
2023-06-03 20:43:00 +0300

    Add new image files to client. Use the image on the AdminPage.vue.

--- client/src/components/AdminPage.vue
+++ client/src/components/AdminPage.vue
@@ -1,12 +1,21 @@
 <template>
   <div class="admin">
+    <h2>Welcome to the secret page</h2>
     <div class="row mx-0 px-0">
       <div class="col-sm-6 text-container">
-        <h2>Welcome to the secret page</h2>
         <p>
           This is a secret page for admin stuff. If you have the credentials,
-          then go right ahead I suppose.
+          then go right ahead. If you don't have credentials, this page isn't
+          very useful for you.
         </p>
+        <div class="image-container">
+          <img
+            class="image"
+            src="@/assets/images/adminlock128.png"
+            alt="Image of a lock"
+          />
+          <p class="image-caption">I sure hope this system is secure.</p>
+        </div>
       </div>
       <div class="col-sm-6 px-0 login-container d-flex justify-content-end">
         <LoginForm />
