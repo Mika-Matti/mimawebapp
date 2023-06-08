2de52b4a6107cec095a5695c317503041c2ac5fb
Mika-Matti Auerkallio <mikamatti.auerkallio@gmail.com>
2023-06-09 01:48:05 +0300

    Move the contents of client component AboutPage to AboutView and delete AboutPage.vue.

--- client/src/views/AboutView.vue
+++ client/src/views/AboutView.vue
@@ -1,18 +1,59 @@
 <template>
   <PageHeader />
-  <AboutPage />
+  <div class="about">
+    <div class="about-content">
+      <div class="text">
+        <h3>Some things about me</h3>
+        <p>
+          I am a computer science student, currently open for work opportunities
+          in Finland.
+        </p>
+        <p>
+          To contact me, you can mail me at
+          <a href="mailto:mima@mimanet.net" target="_blank" rel="noopener"
+            >mima@mimanet.net</a
+          >
+          or send me a message over at
+          <a
+            href="https://www.linkedin.com/in/mika-matti/"
+            target="_blank"
+            rel="noopener"
+            >LinkedIn</a
+          >
+        </p>
+        <p>
+          All my projects showcased here can be found on my
+          <a href="https://github.com/Mika-Matti" target="_blank" rel="noopener"
+            >Github</a
+          >
+          account.
+        </p>
+      </div>
+      <div class="image-container">
+        <img
+          class="image"
+          src="@/assets/images/mima128.png"
+          alt="Self portrait in pixel-art by me"
+        />
+        <p class="image-caption">Cheeky self portrait included.</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
 import PageHeader from "@/components/PageHeader.vue";
-import AboutPage from "@/components/AboutPage.vue"; // @ is an alias to /src
 
 @Options({
   components: {
     PageHeader,
-    AboutPage,
   },
 })
 export default class AboutView extends Vue {}
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+@import "../assets/scss/components.scss";
+</style>
