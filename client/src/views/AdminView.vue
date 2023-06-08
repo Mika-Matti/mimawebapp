5cccc8b1e315988c5bde93e31077456da3014af0
Mika-Matti Auerkallio <mikamatti.auerkallio@gmail.com>
2023-06-09 01:53:41 +0300

    Move the contents of client component AdminPage to AdminView and delete AdminPage.vue

--- client/src/views/AdminView.vue
+++ client/src/views/AdminView.vue
@@ -1,18 +1,45 @@
 <template>
   <PageHeader />
-  <AdminPage />
+  <div class="admin">
+    <h2>Welcome to the secret page</h2>
+    <div class="row mx-0 px-0">
+      <div class="col-sm-6 text-container">
+        <p>
+          This is a secret page for admin stuff. If you have the credentials,
+          then go right ahead. If you do not have credentials, then this page
+          isn't very useful for you.
+        </p>
+        <div class="image-container">
+          <img
+            class="image"
+            src="@/assets/images/adminlock128.png"
+            alt="Image of a lock"
+          />
+          <p class="image-caption">I sure hope this system is secure.</p>
+        </div>
+      </div>
+      <div class="col-sm-6 px-0 login-container d-flex justify-content-end">
+        <LoginForm />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
 import PageHeader from "@/components/PageHeader.vue";
-import AdminPage from "@/components/AdminPage.vue";
+import LoginForm from "@/components/LoginForm.vue";
 
 @Options({
   components: {
     PageHeader,
-    AdminPage,
+    LoginForm,
   },
 })
 export default class AdminView extends Vue {}
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+@import "../assets/scss/components.scss";
+</style>
