2fd3ff2576839e7e2762743732def5ea46306d7c
Mika-Matti Auerkallio <mikamatti.auerkallio@gmail.com>
2023-06-09 01:55:33 +0300

    Move the contents of client component HomePage to HomeView and delete HomePage.vue

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,18 +1,52 @@
 <template>
   <PageHeader />
-  <HomePage />
+  <div class="home">
+    <h3>Welcome to my website</h3>
+    <p>
+      This is my little project website with primary purpose to showcase my
+      projects portfolio. For the website itself I used node.js for server,
+      mySQL for database and VUE for the client as well as typescript for
+      general programming and jest for unit testing.
+    </p>
+    <p>I hope you enjoy your stay.</p>
+    <p>- Mika-Matti</p>
+    <h3>Some essential links</h3>
+    <ul>
+      <li>
+        <a href="https://github.com/Mika-Matti" target="_blank" rel="noopener"
+          >Github</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://www.linkedin.com/in/mika-matti/"
+          target="_blank"
+          rel="noopener"
+          >LinkedIn</a
+        >
+      </li>
+      <li>
+        <a href="mailto:mima@mimanet.net" target="_blank" rel="noopener"
+          >mima@mimanet.net</a
+        >
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
 import PageHeader from "@/components/PageHeader.vue";
-import HomePage from "@/components/HomePage.vue"; // @ is an alias to /src
 
 @Options({
   components: {
     PageHeader,
-    HomePage,
   },
 })
 export default class HomeView extends Vue {}
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+@import "../assets/scss/components.scss";
+</style>
