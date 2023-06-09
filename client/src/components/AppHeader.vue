00806f890ace275d347675df815ee72f2a77fe6b
Mika-Matti Auerkallio <mikamatti.auerkallio@gmail.com>
2023-06-09 05:46:15 +0300

    Create client component UserHeader.vue and use it in AppHeader.vue. Create base feature of displaying text reactively in UserHeader if user is logged in.

--- client/src/components/AppHeader.vue
+++ client/src/components/AppHeader.vue
@@ -1,8 +1,30 @@
 <template>
   <div class="header">
-    <img alt="mimanet logo" src="@/assets/images/mimanetlogo128.png" />
+    <div class="container-fluid">
+      <div class="row px-0">
+        <div class="col px-0"></div>
+        <div class="col px-0">
+          <img alt="mimanet logo" src="@/assets/images/mimanetlogo128.png" />
+        </div>
+        <div class="col px-0">
+          <UserHeader />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import UserHeader from "@/components/UserHeader.vue";
+
+@Options({
+  components: {
+    UserHeader,
+  },
+})
+export default class AppHeader extends Vue {}
+</script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
---
+++ client/src/components/UserHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <div v-if="isAuthenticated" class="user-header">You are logged in</div>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed } from "vue";
+import { useStore } from "vuex";
+
+export default defineComponent({
+  setup() {
+    const store = useStore();
+
+    const isAuthenticated = computed(() => {
+      return store.getters.getIsAuthenticated;
+    });
+
+    return {
+      isAuthenticated,
+    };
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+@import "../assets/scss/components.scss";
+</style>
