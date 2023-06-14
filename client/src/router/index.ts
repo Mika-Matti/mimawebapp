02d1cbe88c7336683f81df86caf5049c0b49b8d6
Mika-Matti Auerkallio <mikamatti.auerkallio@gmail.com>
2023-06-14 09:39:58 +0300

    Add client router guards for routes to edit and create project views.

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -5,48 +5,69 @@ import ProjectView from "../views/ProjectView.vue";
 import AboutView from "../views/AboutView.vue";
 import AdminView from "../views/AdminView.vue";
 import EditorView from "../views/EditorView.vue";
+import store from "../store";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "home",
     component: HomeView,
+    meta: { requiresAuth: false }, // Set requiresAuth to false for public
   },
   {
     path: "/:object/:id/edit",
     name: "edit",
     component: EditorView,
+    meta: { requiresAuth: true }, // Set requiresAuth to true for protected routes
   },
   {
     path: "/create/:object",
     name: "create",
     component: EditorView,
+    meta: { requiresAuth: true }, // Set requiresAuth to true for protected routes
   },
   {
     path: "/projects",
     name: "projects",
     component: ProjectsView,
+    meta: { requiresAuth: false }, // Set requiresAuth to false for public
   },
   {
     path: "/projects/:id",
     name: "project",
     component: ProjectView,
+    meta: { requiresAuth: false }, // Set requiresAuth to false for public
   },
   {
     path: "/about",
     name: "about",
     component: AboutView,
+    meta: { requiresAuth: false }, // Set requiresAuth to false for public
   },
   {
     path: "/admin",
     name: "admin",
     component: AdminView,
+    meta: { requiresAuth: false }, // Set requiresAuth to false for public
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const isAuthenticated = store.getters.getIsAuthenticated;
+  const role = store.getters.getRole;
+  const isAuthorized = () => isAuthenticated.value && role.value === "admin";
+  const requiresAuth = to.matched.some((record) => record.meta.requiresAuth);
+
+  if (requiresAuth && !isAuthorized) {
+    next("/");
+  } else {
+    next();
+  }
 });
 
 export default router;
