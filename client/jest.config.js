5fc588dda5e0d281881c4986cdc2f11132496308
Mika-Matti Auerkallio <mikamatti.auerkallio@gmail.com>
2023-06-25 16:22:25 +0300

    Modify client jest.config.js to exclude test coverage for shims-vue-files and types.ts

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
   collectCoverage: true,
-  collectCoverageFrom: ["src/**/*.{ts,vue}", "!src/main.ts"],
+  collectCoverageFrom: [
+    "src/**/*.{ts,vue}",
+    "!src/main.ts",
+    "!src/shims-vue.d.ts",
+    "!src/shims-vuex.d.ts",
+    "!src/types.ts",
+  ],
   transformIgnorePatterns: ["node_modules/(?!axios)"],
   coverageProvider: "v8",
 };
