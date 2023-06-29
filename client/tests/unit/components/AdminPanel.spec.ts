d61a1add923671e84d6869f94d9af4922a4a1730
Mika-Matti Auerkallio <mikamatti.auerkallio@gmail.com>
2023-06-29 16:36:10 +0300

    Add client unit test file AdminPanel.spec.ts and write test cases for rendering adminpanel when logged in and out.

---
+++ client/tests/unit/components/AdminPanel.spec.ts
@@ -0,0 +1,49 @@
+import { mount } from "@vue/test-utils";
+import { createStore } from "vuex";
+import AdminPanel from "@/components/AdminPanel.vue";
+
+describe("AdminPanel-component", () => {
+  it("should not render adminpanel when not logged in", () => {
+    const store = createStore({
+      getters: {
+        getIsAuthenticated: () => false,
+        getUsername: () => "",
+        getRole: () => "guest",
+        getExpiration: () => 0,
+      },
+    });
+
+    const mountConfig = {
+      global: {
+        plugins: [store],
+      },
+    };
+
+    const wrapper = mount(AdminPanel, mountConfig);
+
+    expect(wrapper.find(".admin-panel").exists()).toBe(false);
+    expect(wrapper.find(".view-options").exists()).toBe(false);
+  }); // Test case ends
+
+  it("should render adminpanel when user is logged in", () => {
+    const store = createStore({
+      getters: {
+        getIsAuthenticated: () => true,
+        getUsername: () => "JohnDoe",
+        getRole: () => "admin",
+        getExpiration: () => 360000,
+      },
+    });
+
+    const mountConfig = {
+      global: {
+        plugins: [store],
+      },
+    };
+
+    const wrapper = mount(AdminPanel, mountConfig);
+
+    expect(wrapper.find(".admin-panel").exists()).toBe(true);
+    expect(wrapper.find(".view-options").exists()).toBe(true);
+  }); // Test case ends
+});
