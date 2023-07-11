6275e4c21ebc0d829466f3bd6c9a28621612fe29
Mika-Matti Auerkallio <mikamatti.auerkallio@gmail.com>
2023-07-11 06:10:52 +0300

    Create client test file for AdminView and write a test case to assert proper rendering of the view

---
+++ client/tests/unit/views/AdminView.spec.ts
@@ -0,0 +1,14 @@
+import { shallowMount } from "@vue/test-utils";
+import AdminView from "@/views/AdminView.vue";
+import PageHeader from "@/components/PageHeader.vue";
+import LoginForm from "@/components/LoginForm.vue";
+
+describe("AdminView", () => {
+  it("should render the view properly", () => {
+    const wrapper = shallowMount(AdminView);
+
+    expect(wrapper.find(".admin").exists()).toBe(true);
+    expect(wrapper.findComponent(PageHeader).exists()).toBe(true);
+    expect(wrapper.findComponent(LoginForm).exists()).toBe(true);
+  });
+});
