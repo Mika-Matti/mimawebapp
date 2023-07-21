4acdbf53eaf6f868c43a2eb552431731661f1bd8
Mika-Matti Auerkallio <mikamatti.auerkallio@gmail.com>
2023-07-21 13:32:11 +0300

    Update sql createDB script with a new table for posts

--- sql/createDB.sql
+++ sql/createDB.sql
@@ -1,12 +1,6 @@
-#Check if the database exists
-SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = 'mima_test_db';
-
 CREATE DATABASE IF NOT EXISTS mima_test_db;
 
 USE mima_test_db;
-
-#Check if the table exists
-SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'mima_test_db' AND TABLE_NAME = 'projects';
 
 CREATE TABLE IF NOT EXISTS projects (
   project_id INT(11) NOT NULL AUTO_INCREMENT,
@@ -18,8 +12,15 @@ CREATE TABLE IF NOT EXISTS projects (
   PRIMARY KEY (project_id)
 );
 
-#Check if the table exists
-SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'mima_test_db' AND TABLE_NAME = 'users';
+CREATE TABLE IF NOT EXISTS projects (
+  project_id INT(11) NOT NULL AUTO_INCREMENT,
+  project_title VARCHAR(255) NOT NULL,
+  project_description TEXT NOT NULL,
+  project_content TEXT NOT NULL,
+  project_link VARCHAR(255),
+  project_start_date DATE,
+  PRIMARY KEY (project_id)
+);
 
 CREATE TABLE IF NOT EXISTS users (
 	user_id INT(11) NOT NULL AUTO_INCREMENT,
@@ -28,4 +29,15 @@ CREATE TABLE IF NOT EXISTS users (
 	user_email VARCHAR(255),
 	user_role ENUM('admin', 'moderator', 'user', 'guest'),
 	PRIMARY KEY (user_id)
+);
+
+CREATE TABLE IF NOT EXISTS posts (
+  post_id INT(11) NOT NULL AUTO_INCREMENT,
+  post_title VARCHAR(255) NOT NULL,
+  post_content TEXT NOT NULL,
+  post_date DATE,
+  post_is_public BOOLEAN,
+  user_id INT(11),
+  PRIMARY KEY (post_id),
+  FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
