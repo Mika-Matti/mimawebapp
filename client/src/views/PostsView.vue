20ae5ef14c78ab72425d6bcbba2e9aac81c1cfdd
Mika-Matti Auerkallio <mikamatti.auerkallio@gmail.com>
2023-07-25 02:17:05 +0300

    Create client page for posts.

---
+++ client/src/views/PostsView.vue
@@ -0,0 +1,62 @@
+<template>
+  <PageHeader />
+  <AdminPanel>
+    <template #buttons>
+      <router-link class="button ms-0 my-0" :to="`/create/post`">
+        new post
+      </router-link>
+    </template>
+  </AdminPanel>
+  <div class="posts">
+    <ul>
+      <li v-for="post in posts" :key="post.post_id">
+        <PostNode :post="post" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import { useStore } from "vuex";
+import { Post } from "@/types";
+import PageHeader from "@/components/ui/PageHeader.vue";
+import AdminPanel from "@/components/ui/AdminPanel.vue";
+import PostNode from "@/components/PosttNode.vue";
+
+@Options({
+  components: {
+    PageHeader,
+    AdminPanel,
+    PostNode,
+  },
+})
+export default class PostsView extends Vue {
+  pageHeader = "/";
+  posts: Post[] = [];
+  store = useStore();
+
+  // Fetch posts from server
+  async fetchPosts() {
+    try {
+      await this.store.dispatch(
+        "fetchPosts",
+        this.store.getters.getIsAuthenticated
+      );
+      this.posts = this.store.getters.getPosts;
+    } catch (error) {
+      //console.error("Failed to fetch posts", error);
+    }
+  }
+
+  // Call fetch once component is mounted
+  mounted() {
+    this.fetchPosts();
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+@import "../assets/scss/components.scss";
+</style>
