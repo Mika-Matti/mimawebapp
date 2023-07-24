81dcd5eede964de644dcfe887f9ce8d4054b78a4
Mika-Matti Auerkallio <mikamatti.auerkallio@gmail.com>
2023-07-25 02:25:42 +0300

    Create client page for singular post

---
+++ client/src/views/PostView.vue
@@ -0,0 +1,88 @@
+<template>
+  <PageHeader />
+  <AdminPanel>
+    <template #buttons>
+      <router-link class="button ms-0 my-0" :to="`/edit/post/${post_id}`">
+        edit post
+      </router-link>
+      <button class="button my-0" @click="confirmDelete">delete post</button>
+    </template>
+  </AdminPanel>
+  <ProjectDisplay v-if="post" :post="post" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { useStore } from "vuex";
+import { Post } from "@/types";
+import PageHeader from "@/components/ui/PageHeader.vue";
+import AdminPanel from "@/components/ui/AdminPanel.vue";
+import PostDisplay from "@/components/PostDisplay.vue";
+
+export default defineComponent({
+  components: {
+    PageHeader,
+    AdminPanel,
+    PostDisplay,
+  },
+  data() {
+    return {
+      store: useStore(),
+      post_id: "-1",
+      post: {
+        post_title: "",
+        post_description: "",
+        post_content: "",
+        post_date: new Date(),
+        post_is_public: true,
+      } as Post,
+    };
+  },
+  methods: {
+    // Fetch post by id from server
+    async fetchPostById(id: string) {
+      try {
+        const isAuth = this.store.getters.getIsAuthenticated;
+        await this.store.dispatch(`fetchPostById`, {
+          isAuth,
+          id,
+        });
+        this.post = this.store.getters.getPost;
+
+        if (!this.post) {
+          this.$router.push({ name: "NotFound" });
+        }
+      } catch (error) {
+        //console.error("Failed to fetch post by id", error);
+      }
+    },
+    // Delete project methods
+    confirmDelete() {
+      if (
+        confirm("Are you sure you want to delete this post from the database?")
+      ) {
+        this.deletePostById();
+      }
+    },
+    async deletePostById() {
+      try {
+        await this.store.dispatch(`deletePostById`, this.post_id);
+        // Redirect to posts page after deletion
+        this.$router.push("/posts");
+      } catch (error) {
+        //console.error("Failed to delete post by id", error);
+      }
+    },
+  },
+
+  mounted() {
+    this.post_id = this.$route.params.id.toString();
+    this.fetchPostById(this.post_id);
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+@import "../assets/scss/components.scss";
+</style>
