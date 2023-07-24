024427a0d576156e8ad441b6b3bfd396a028e3fa
Mika-Matti Auerkallio <mikamatti.auerkallio@gmail.com>
2023-07-25 01:31:14 +0300

    Create client component PostNode for display posts in

---
+++ client/src/components/PostNode.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="post-item container-fluid mx-0 px-0">
+    <div class="row mx-0 px-0">
+      <div class="col mx-0 px-0">
+        <div class="row mx-0 px-0 post-title">
+          <h2>{{ post.post_title }}</h2>
+        </div>
+        <div v-if="post.post_date" class="row mx-0 px-0 post-date">
+          <h3>
+            POSTED:
+            {{ post.post_date.toLocaleDateString("en-GB") }}
+          </h3>
+        </div>
+        <div class="row mx-0 px-0 post-content">
+          <p>{{ post.post_content }}</p>
+        </div>
+      </div>
+      <div class="col-auto mx-0 px-0 d-flex flex-column justify-content-center">
+        <AdminPanel>
+          <template #buttons>
+            <router-link
+              class="button ms-0 my-0"
+              :to="`/edit/post/${post.post_id}`"
+            >
+              edit
+            </router-link>
+            <button class="button me-0 my-0" @click="confirmDelete">
+              delete
+            </button>
+          </template>
+        </AdminPanel>
+        <router-link
+          class="post-link post-button"
+          :to="`/posts/${post.post_id}`"
+        >
+          View Post
+        </router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import { useStore } from "vuex";
+import { Post } from "@/types";
+import AdminPanel from "@/components/AdminPanel.vue";
+
+@Options({
+  props: {
+    post: {},
+  },
+  components: {
+    AdminPanel,
+  },
+})
+export default class PostNode extends Vue {
+  post!: Post;
+  store = useStore();
+
+  // Delete post methods
+  confirmDelete() {
+    if (
+      confirm("Are you sure you want to delete this post from the database?")
+    ) {
+      this.deletePostById();
+    }
+  }
+
+  async deletePostById() {
+    try {
+      await this.store.dispatch(`deletePostById`, this.post.post_id);
+      // Redirect to posts page after deletion
+      this.$router.push("/posts");
+    } catch (error) {
+      //console.error("Failed to delete post by id", error);
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+@import "../assets/scss/components.scss";
+</style>
