d70497eeb768846cb9125a9767b00a322e4e115c
Mika-Matti Auerkallio <mikamatti.auerkallio@gmail.com>
2023-08-02 09:15:27 +0300

    Write at least one test case for every method in posts.controller.test.ts

--- server/__tests__/controllers/post.controller.test.ts
+++ server/__tests__/controllers/post.controller.test.ts
@@ -233,3 +233,234 @@ describe("GET /posts/all", () => {
     ]);
   });
 }); // End of GET /posts/all
+
+describe("GET /posts/public/:id", () => {
+  beforeAll(() => {
+    mockQuery.mockImplementation((sql: string, values?: any[]) => {
+      return new Promise<any>((resolve, reject) => {
+        const expectedId: string = "2";
+        const expectedQuery: string =
+          "SELECT * FROM posts WHERE post_id = ? AND post_is_public = true";
+        const testId: string = values && values.length > 0 ? values[0] : "-1";
+
+        if (sql === expectedQuery && testId === expectedId) {
+          resolve([
+            {
+              post_id: 2,
+              post_title: "Post B",
+              post_content: "Lorem ipsum dolor sit amet...",
+              post_date: "2023-01-22 21:22:00",
+              post_is_public: true,
+              user_id: 1,
+            },
+          ]);
+        } else {
+          reject("Error: Mock function got wrong query");
+        }
+      });
+    });
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should return a Post-object of the same id as was given (2)", async () => {
+    const req: Request = {
+      params: {
+        id: "2",
+      },
+    } as unknown as Request;
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    } as unknown as Response;
+
+    await getPublicPostById(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith({
+      post_id: 2,
+      post_title: "Post B",
+      post_content: "Lorem ipsum dolor sit amet...",
+      post_date: "2023-01-22 21:22:00",
+      post_is_public: true,
+      user_id: 1,
+    });
+  });
+}); // End of GET /posts/public/:id
+
+describe("GET /posts/public/:id", () => {
+  beforeAll(() => {
+    mockQuery.mockImplementation((sql: string, values?: any[]) => {
+      return new Promise<any>((resolve, reject) => {
+        const expectedId: string = "3";
+        const expectedQuery: string = "SELECT * FROM posts WHERE post_id = ?";
+        const testId: string = values && values.length > 0 ? values[0] : "-1";
+
+        if (sql === expectedQuery && testId === expectedId) {
+          resolve([
+            {
+              post_id: 3,
+              post_title: "Post C",
+              post_content: "Lorem ipsum dolor sit amet...",
+              post_date: "2023-01-23 21:22:00",
+              post_is_public: false,
+              user_id: 1,
+            },
+          ]);
+        } else {
+          reject("Error: Mock function got wrong query");
+        }
+      });
+    });
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should return a Post-object of the same id as was given (3)", async () => {
+    const req: Request = {
+      params: {
+        id: "3",
+      },
+    } as unknown as Request;
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    } as unknown as Response;
+
+    await getAnyPostById(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith({
+      post_id: 3,
+      post_title: "Post C",
+      post_content: "Lorem ipsum dolor sit amet...",
+      post_date: "2023-01-23 21:22:00",
+      post_is_public: false,
+      user_id: 1,
+    });
+  });
+}); // End of GET /posts/all/:id
+
+describe("PUT /posts/all/:id", () => {
+  beforeAll(() => {
+    mockQuery.mockImplementation((sql: string, values?: any[]) => {
+      return new Promise<any>((resolve, reject) => {
+        let testPost: Post;
+        let testId: string;
+
+        const expectedPost: Post = {
+          post_title: "Post B",
+          post_content: "Lorem ipsum dolor sit amet...",
+          post_date: "2023-01-22 21:22:00",
+          post_is_public: true,
+        };
+        const expectedId: string = "2";
+        const expectedQuery: string = "UPDATE posts SET ? WHERE post_id = ?";
+
+        if (values && values.length >= 2) {
+          [testPost, testId] = values;
+
+          for (const [key, value] of Object.entries(expectedPost)) {
+            if (expectedPost[key] != value) {
+              reject(
+                `Error: Given test value: ${key}:${testPost[key]} for update doesn't match expected: ${key}:${value}`
+              );
+            }
+          }
+
+          if (sql === expectedQuery && expectedId === testId) {
+            resolve({ affectedRows: 1 });
+          } else {
+            reject("Error: Mock function got wrong query");
+          }
+        } else if (values && values.length === 1) {
+          [testId] = values;
+          if (
+            sql === "SELECT post_date FROM posts WHERE post_id = ?" &&
+            expectedId === testId
+          ) {
+            resolve([{ post_date: "2023-01-22 21:22:00" }]);
+          }
+        } else {
+          reject("Error: 'values' array is missing or has insufficient length");
+        }
+      });
+    });
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should return the updated Post-object", async () => {
+    const req: Request = {
+      params: {
+        id: "2",
+      },
+      body: {
+        post_title: "Post B updated title",
+        post_content: "Lorem ipsum dolor sit amet...",
+        post_date: "2023-01-22 21:22:00",
+        post_is_public: true,
+      },
+    } as unknown as Request;
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    } as unknown as Response;
+
+    await updatePost(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith({
+      post_id: 2,
+      post_title: "Post B updated title",
+      post_content: "Lorem ipsum dolor sit amet...",
+      post_date: "2023-01-22 21:22:00",
+      post_is_public: true,
+    });
+  });
+}); // End of PUT /posts/all/:id
+
+describe("DELETE /posts/all/:id", () => {
+  beforeAll(() => {
+    mockQuery.mockImplementation((sql: string, values?: any[]) => {
+      return new Promise<any>((resolve, reject) => {
+        const expectedId: string = "1";
+        const expectedQuery: string = "DELETE FROM posts WHERE post_id = ?";
+        const testId: string = values && values.length > 0 ? values[0] : "-1";
+
+        if (sql === expectedQuery && testId === expectedId) {
+          resolve({ affectedRows: 1 });
+        } else {
+          reject("Error: Mock function got wrong query");
+        }
+      });
+    });
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should delete the post with given id and return 204 message", async () => {
+    const req: Request = {
+      params: {
+        id: "1",
+      },
+    } as unknown as Request;
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+      send: jest.fn(),
+    } as unknown as Response;
+
+    await deletePost(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(204);
+  });
+}); // End of DELETE /posts/all
